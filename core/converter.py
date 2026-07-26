c8e59f6645c5c0e31eae8ed76b90a65fed0d380d
Mauli Patel <124795820+itsmemauliii@users.noreply.github.com>
2026-07-26 13:26:42 +0530

    Update converter.py

--- core/converter.py
+++ core/converter.py
@@ -1,16 +1,14 @@
 from core.image_processor import preprocess
-from core.ocr_engine import detect_text
-from core.layout_detector import detect_layout
+from core.ocr_engine import extract_text
 from core.ppt_builder import build_ppt
 
-def convert(image):
 
-    processed=preprocess(image)
+def convert_image(image):
 
-    text=detect_text(processed)
+    processed = preprocess(image)
 
-    layout=detect_layout(processed)
+    text = extract_text(processed)
 
-    ppt=build_ppt(image,text,layout)
+    ppt = build_ppt(image,text)
 
     return ppt
