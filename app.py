231f0d574ad0dfb64f2facc118d05a785ef6ffd1
Mauli Patel <124795820+itsmemauliii@users.noreply.github.com>
2026-07-26 13:23:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,15 @@
 import streamlit as st
-from PIL import Image
-import os
-
-from core.ocr import extract_text
-from core.ppt_generator import create_ppt
 
 st.set_page_config(
     page_title="Image2PPT AI",
     page_icon="📊",
     layout="wide"
 )
-st.title("Image2PPT AI")
 
-tab1,tab2=st.tabs([
-    "Convert",
-    "History"
-])
+home = st.Page("pages/upload.py", title="Upload", icon="📤")
+history = st.Page("pages/history.py", title="History", icon="🕘")
+settings = st.Page("pages/settings.py", title="Settings", icon="⚙️")
 
-st.title("📊 Image2PPT AI")
-st.caption("Convert images into editable PowerPoint slides")
+pg = st.navigation([home, history, settings])
 
-uploaded = st.file_uploader(
-    "Upload Image",
-    type=["png","jpg","jpeg"]
-)
-
-if uploaded:
-
-    image = Image.open(uploaded)
-
-    st.image(image,use_container_width=True)
-
-    if st.button("Generate PowerPoint"):
-
-        with st.spinner("Reading image..."):
-
-            text_blocks = extract_text(image)
-
-            output_path = create_ppt(image,text_blocks)
-
-        with open(output_path,"rb") as f:
-
-            st.download_button(
-                "Download PPT",
-                f,
-                file_name="presentation.pptx"
-            )
+pg.run()
